1c78e7ab810d13b24accab28df6aeeb5487d48ad
Demóstenes Acoglanis <126527444+Ademostenes@users.noreply.github.com>
2024-05-19 22:06:52 -0300

    Add files via upload

--- Js/funcBusquedas.js
+++ Js/funcBusquedas.js
@@ -10,4 +10,21 @@ function cambiaBusqueda(){
     }
 }
 
+
+
+function capaBusquedas(nombre){
+let capa = document.getElementById(nombre);
+if (nombre == "origen"){
+    document.getElementById("destino").display = "none"; 
+} else {
+    document.getElementById("origen").display = "none";
+}
+if (capa.style.display == "none"){
+    capa.style.display = "flex";
+} else {
+    capa.style.display = "none";
+}
+}
+
 cambiaBusqueda();
+
